c98984cda4085bc9b4382aff1d7bc4231dcc7247
Kavin.S <senthilkavi1972@gmail.com>
2025-11-22 21:20:29 +0530

    Add dynamic pattern printing in Dynamicpattern class
    Implement a dynamic pattern printing program that outputs a sequence of numbers in a triangular format.

--- pattern6.java
+++ pattern6.java
@@ -1,1 +1,22 @@
+public class Dynamicpattern {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=0;
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(++count+" ");
+			}System.out.println();
+		}
+
+	}
+
+}
+
+output:
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+16 17 18 19 20 21 
