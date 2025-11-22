45707f0b8d8d2af5075ce4768b10412e48f88f49
Kavin.S <senthilkavi1972@gmail.com>
2025-11-22 21:30:55 +0530

    Update pattern7.java

--- pattern7.java
+++ pattern7.java
@@ -21,3 +21,28 @@ output:
 **
 *
 
+
+	//Another method
+public class Dynamicpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int count=0;
+		int n=6;
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=n-(i-1);j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+
+	}
+
+}
+******
+*****
+****
+***
+**
+*
+
+
