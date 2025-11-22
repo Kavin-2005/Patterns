29d1c72f9d387de9299a8dd3257bfeff716a7785
Kavin.S <senthilkavi1972@gmail.com>
2025-11-22 21:00:45 +0530

    Update pattern4.java

--- pattern4.java
+++ pattern4.java
@@ -1,1 +1,20 @@
+public class Dynamicpattern {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}System.out.println();
+		}
+
+	}
+
+}
+output:
+1
+12
+123
+1234
+12345
+123456
