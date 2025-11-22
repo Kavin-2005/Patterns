6ea38fde94c19f53b29db807c7929d9d866d69eb
Kavin.S <senthilkavi1972@gmail.com>
2025-11-22 20:59:24 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -1,1 +1,20 @@
+public class Dynamicpattern {
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=6;j++) {
+				System.out.print(i);
+			}System.out.println();
+		}
+
+	}
+
+}
+output:
+111111
+222222
+333333
+444444
+555555
+666666
 
