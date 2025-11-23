695a3ed21a2cbfc048a9e1097e4cecd26c985c0e
Kavin.S <senthilkavi1972@gmail.com>
2025-11-23 10:20:48 +0530

    Implement pattern printing in pattern10.java

---
+++ pattern10.java
@@ -0,0 +1,15 @@
+public class jpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=6;
+		
+		for(int i=1;i<=(n*2)-1;i++) {
+			int rowchange=i>n?((2*n)-i):i;
+			for(int j=1;j<=rowchange;j++) {
+				System.out.print("*");
+			}System.out.println();
+
+}
+		}
+}
