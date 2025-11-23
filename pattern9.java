87834adf77e96c65f2e3b41f653af18e2deed7b7
Kavin.S <senthilkavi1972@gmail.com>
2025-11-23 10:05:17 +0530

    Add Dynamicpattern class to print number pattern

---
+++ pattern9.java
@@ -0,0 +1,15 @@
+public class Dynamicpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int count=6;
+		int n=6;
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=n-(i-1);j++) {
+				System.out.print(j);
+			}System.out.println();
+		}
+
+	}
+
+}
